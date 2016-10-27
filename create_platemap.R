eb0ee1fb73a97fa6341156a8eb044bd45afee274
Shantanu Singh <shsingh@broadinstitute.org>
2016-10-26 22:28:09 -0400

    write_tsv

--- create_platemap.R
+++ create_platemap.R
@@ -2,9 +2,7 @@ library(tidyverse)
 library(stringr)
 
 platemap <- 
-  read_csv("../../metadata/CRISPR_PILOT_B1/platemap/DEPENDENCIES1_raw.csv")
-
-platemap %>%
+  read_csv("../../metadata/CRISPR_PILOT_B1/platemap/DEPENDENCIES1_raw.csv") %>%
   gather(WellCol, pert_name, -Row) %>%
   rename(WellRow = Row) %>%
   rowwise() %>%
@@ -12,5 +10,10 @@ platemap %>%
   mutate(gene_name = str_split(pert_name, "-")[[1]][1]) %>%
   ungroup() %>%
   select(WellRow, WellCol, well_position, gene_name, pert_name) %>%
-  arrange(well_position) %>%
+  arrange(well_position) 
+
+platemap %>%
   write_csv("../../metadata/CRISPR_PILOT_B1/platemap/DEPENDENCIES1.csv")
+
+platemap %>%
+  write_tsv("../../metadata/CRISPR_PILOT_B1/platemap/DEPENDENCIES1.txt")
