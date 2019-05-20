1e0246ed96bf8db2ffb4bba1f8e4fe53be91825c
Greg Way <gregory.way@gmail.com>
2019-05-20 15:28:18 -0400

    Adding Platemap Visualization (#10)
    * add r-platetools to env
    
    also updating other packages to accomodate
    
    * add platemap viz to bash
    
    * change mod of file
    
    * closes #9
    
    * ignore r plots
    
    * add visualize platemap notebook
    
    * rerun audit_analysis bash script

--- 1.analyze-audits/audit_analysis.sh
+++ 1.analyze-audits/audit_analysis.sh
@@ -8,9 +8,13 @@
 # Step 0 - Convert all notebooks to scripts
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/nbconverted *.ipynb
 
-# Step 1 - Analyze audites
+# Step 1 - Analyze audits
 jupyter nbconvert --to=html \
         --FilesWriter.build_directory=scripts/html \
         --ExecutePreprocessor.kernel_name=python3 \
         --ExecutePreprocessor.timeout=10000000 \
         --execute analyze-audit.ipynb
+        
+# Step 2 - Visualize platemaps
+Rscript --vanilla scripts/nbconverted/visualize-platemaps.r 
+
