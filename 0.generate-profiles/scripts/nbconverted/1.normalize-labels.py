36d5dcf3a37cf1cd4e33f17198e181782696e201
Greg Way <gregory.way@gmail.com>
2019-08-06 18:43:38 -0400

    Adding Normalization Notebook for Cell Health Target Variables (#36)
    * update merge and plot missingness
    
    * update cell health labels and num guides
    
    * add notebook to normalize data by plate
    
    * add normalized label data
    
    * update normalization from mean absolute to median absolute
    
    * update normalized variables

---
+++ 0.generate-profiles/scripts/nbconverted/1.normalize-labels.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Normalize Cell Health Labels
+# 
+# **Gregory Way, 2019**
+
+# In[1]:
+
+
+import os
+import numpy as np
+from scipy.stats import median_absolute_deviation
+import pandas as pd
+
+
+# In[2]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+# Function to scale cell health target variables
+def mad_scale(x):
+    x_median = np.nanmedian(x)
+    x_mad = median_absolute_deviation(x, nan_policy="omit")
+    x_mad_scale = (x - x_median) / x_mad
+    return x_mad_scale
+
+
+# In[4]:
+
+
+file = os.path.join("data", "labels", "cell_health_labels.tsv")
+label_df = pd.read_csv(file, sep='\t')
+
+print(label_df.shape)
+label_df.head(2)
+
+
+# In[5]:
+
+
+# Apply normalization by plate
+normalized_label_df = (
+    label_df
+    .drop(["guide", "well_col", "well_row"], axis="columns")
+    .groupby(["cell_id", "plate_name"])
+    .transform(mad_scale)
+)
+
+normalized_label_df = pd.concat(
+    [
+        label_df.loc[:, ["cell_id", "guide", "plate_name", "well_col", "well_row"]],
+        normalized_label_df
+    ],
+    axis="columns"
+)
+
+print(normalized_label_df.shape)
+normalized_label_df.head(2)
+
+
+# In[6]:
+
+
+# Write to file
+file = os.path.join("data", "labels", "normalized_cell_health_labels.tsv")
+normalized_label_df.to_csv(file, index=False, sep='\t')
+
