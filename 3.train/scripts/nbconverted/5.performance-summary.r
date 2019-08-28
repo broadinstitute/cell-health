e53859a909c03df1765888a6f172ca1d50945796
Greg Way <gregory.way@gmail.com>
2019-08-28 10:59:00 -0400

    Retraining Models with Pycytominer Profiles (#59)
    * add dask-ml and gridextra back to env
    
    * add models trained with pycytominer data
    
    * update figures for pycytominer data
    
    * update results for pycytomienr
    
    * update training and testing data for pycytominer
    
    * update data stratification to read in files from multiple directories
    
    * rerun pipeline with pycytominer data
    
    * remove redundant space
    
    * compare performance between cytominer and pycytominer
    
    * minor fix to performance suymmary
    
    * add pycytominer comparison

--- 3.train/scripts/nbconverted/5.performance-summary.r
+++ 3.train/scripts/nbconverted/5.performance-summary.r
@@ -1,7 +1,6 @@
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(ggrepel))
-suppressPackageStartupMessages(library(ggpmisc))
 
 results_dir <- "results"
 
@@ -75,14 +74,23 @@ metric_df$maria_thumbs_up <- tidyr::replace_na(metric_df$maria_thumbs_up, 0)
 metric_df$measurement <- tidyr::replace_na(metric_df$measurement, "other")
 
 metric_df$measurement <- factor(metric_df$measurement,
-                                levels = c("apoptosis", "death", "cell_viability", "toxicity", "dna_damage",
-                                           "cell_cycle_arrest", "g1_arrest", "g2_arrest", "g2_m_arrest",
-                                           "s_arrest", "other"))
+                                levels = c("apoptosis",
+                                           "death",
+                                           "cell_viability",
+                                           "toxicity",
+                                           "dna_damage",
+                                           "cell_cycle_arrest",
+                                           "g1_arrest",
+                                           "g2_arrest",
+                                           "g2_m_arrest",
+                                           "s_arrest",
+                                           "other"))
 print(dim(metric_df))
 head(metric_df, 3)
 
 ggplot(metric_df, aes(x = AUROC, y = mse)) +
-    geom_point(alpha = 0.7,
+    geom_point(alpha = 0.95,
+               size = 2,
                aes(color = measurement,
                    shape = as.factor(maria_thumbs_up))) +
     xlab("Classification\nAUROC (Test Set)") +
@@ -93,17 +101,17 @@ ggplot(metric_df, aes(x = AUROC, y = mse)) +
     scale_shape_manual(name = "Maria :+1:",
                        values = c(16, 3)) +
     scale_color_manual(name = "Measurement",
-                       values = c("apoptosis" = "#ff0000",
-                                  "cell_cycle_arrest" = "#ff5900",
-                                  "cell_viability" = "#ff9900",
-                                  "death" = "#ffcc00",
-                                  "dna_damage" = "#E8B360", 
-                                  "g1_arrest" = "#00c20d",
-                                  "g2_arrest" = "#008f4c",
+                       values = c("apoptosis" = "#a6cee3",
+                                  "cell_cycle_arrest" = "#1f78b4",
+                                  "cell_viability" = "#b2df8a",
+                                  "death" = "#33a02c",
+                                  "dna_damage" = "#fb9a99", 
+                                  "g1_arrest" = "#fdbf6f",
+                                  "g2_arrest" = "#ff7f00",
                                   "g2_m_arrest" = "#005c8c",
                                   "other" = "black",
-                                  "s_arrest" = "#0033bf",
-                                  "toxicity" = "#feab00"),
+                                  "s_arrest" = "#cab2d6",
+                                  "toxicity" = "#6a3d9a"),
                        labels = c("apoptosis" = "Apoptosis",
                                   "cell_cycle_arrest" = "Cell Cycle Arrest",
                                   "cell_viability" = "Cell Viability",
@@ -119,3 +127,200 @@ ggplot(metric_df, aes(x = AUROC, y = mse)) +
 
 file <- file.path("figures", "performance_summary.png")
 ggsave(file, dpi = 300, width = 6, height = 4.5)
+
+# Cytominer results are archived on github
+hash = "26d1095c209d402102494c0c28e978476643e572"
+cyto_file = paste0("https://github.com/broadinstitute/cell-health/raw/",
+                   hash, "/3.train/results/full_cell_health_regression_results.tsv.gz")
+
+cyto_regression_df = readr::read_tsv(cyto_file, col_types=readr::cols()) %>%
+    dplyr::mutate(package = "cytominer")
+
+head(cyto_regression_df, 2)
+
+regression_metrics_df <- regression_metrics_df %>%
+    dplyr::mutate(package = "pycytominer")
+
+head(regression_metrics_df, 2)
+
+all_regression_df <- regression_metrics_df %>%
+    dplyr::inner_join(cyto_regression_df,
+                      by = c("metric", "target", "data_fit", "shuffle", "y_transform"),
+                      suffix = c("_pycytominer", "_cytominer")) %>%
+    dplyr::mutate(mse_diff = mse_pycytominer - mse_cytominer)
+
+head(all_regression_df, 2)
+
+for (metric in unique(all_regression_df$metric)) {
+    all_regression_subset_df <- all_regression_df %>%
+        dplyr::filter(metric == metric)
+    
+    if (metric == "mse") {
+        all_regression_subset_df$mse_pycytominer <- abs(all_regression_subset_df$mse_pycytominer)
+        all_regression_subset_df$mse_cytominer <- abs(all_regression_subset_df$mse_cytominer)
+    }
+    
+    mse_gg <- ggplot(all_regression_subset_df,
+       aes(x=mse_pycytominer, 
+           y=mse_cytominer)) +
+    geom_point(aes(color = data_fit),
+               alpha = 0.7,
+               size = 0.8) +
+    facet_grid(y_transform~shuffle) +
+    xlab("pycytominer") +
+    ylab("cytominer") +
+    ggtitle(metric) +
+    geom_abline(intercept = 0,
+                slope = 1,
+                linetype = "dashed",
+                color = "black",
+                alpha = 0.7) +
+    theme_bw() +
+    theme(strip.background = element_rect(colour = "black",
+                                          fill = "#fdfff4"))
+    
+    print(mse_gg)
+    outfile <- file.path("figures", paste0("compare_pycytominer_cytominer_", metric, ".png"))
+    ggsave(outfile, height = 5, width = 6, dpi = 300)
+}
+
+
+
+all_regression_df %>%
+    dplyr::group_by(metric, data_fit, shuffle, y_transform) %>%
+    dplyr::mutate(percent_pycytominer_better = sum(mse_diff > 0) / n()) %>%
+    dplyr::distinct(metric, data_fit, shuffle, y_transform, percent_pycytominer_better)
+
+# Cytominer results are archived on github
+cyto_file = paste0("https://github.com/broadinstitute/cell-health/raw/",
+                   hash, "/3.train/results/full_cell_health_roc_results.tsv.gz")
+
+cyto_roc_df = readr::read_tsv(cyto_file, col_types=readr::cols())
+
+head(cyto_roc_df, 2)
+
+cyto_file = paste0("https://github.com/broadinstitute/cell-health/raw/",
+                   hash, "/3.train/results/full_cell_health_pr_results.tsv.gz")
+
+cyto_pr_df = readr::read_tsv(cyto_file, col_types=readr::cols())
+
+head(cyto_pr_df, 2)
+
+cyto_auroc_df <- cyto_roc_df %>%
+    dplyr::distinct(metric, target, auc, data_fit,
+                    shuffle, y_transform, min_class_count)
+
+cyto_aupr_df <- cyto_pr_df %>%
+    dplyr::distinct(metric, target, auc, data_fit,
+                    shuffle, y_transform, min_class_count)
+
+cyto_auc_df <- dplyr::bind_rows(cyto_auroc_df, cyto_aupr_df) %>%
+    dplyr::filter(shuffle == "shuffle_false",
+                  data_fit == "test")
+
+cyto_auc_df$metric <- dplyr::recode_factor(
+    cyto_auc_df$metric,
+    "roc" = "AUROC",
+    "aupr" = "AUPR"
+)
+
+cyto_auc_df <- cyto_auc_df %>%
+    tidyr::spread(key = "metric", value = "auc") %>%
+    dplyr::select(-y_transform) %>%
+    dplyr::mutate(package = "pycytominer")
+
+print(dim(cyto_auc_df))
+head(cyto_auc_df, 2)
+
+auc_df <- auc_df %>%
+    dplyr::mutate(package = "cytominer")
+
+all_classification_df <- auc_df %>%
+    dplyr::inner_join(cyto_auc_df,
+                      by = c("target", "data_fit", "shuffle", "min_class_count"),
+                      suffix = c("_pycytominer", "_cytominer")) %>%
+    dplyr::mutate(auroc_diff = AUROC_pycytominer - AUROC_cytominer,
+                  aupr_diff = AUPR_pycytominer - AUPR_cytominer) %>%
+    dplyr::filter(data_fit == "test",
+                  shuffle == "shuffle_false") 
+
+head(all_classification_df, 2)
+
+metric <- "AUROC"
+
+label_logic <- abs(all_classification_df$auroc_diff) > 0.08
+
+ggplot(all_classification_df,
+       aes(x=AUROC_pycytominer, 
+           y=AUROC_cytominer)) +
+    geom_point(alpha = 0.7,
+               size = 0.8) +
+    xlab("pycytominer") +
+    ylab("cytominer") +
+    geom_hline(yintercept = 0.5,
+               linetype = "dashed",
+               color = "red",
+               alpha = 0.7) +
+    geom_vline(xintercept = 0.5,
+               linetype = "dashed",
+               color = "red",
+               alpha = 0.7) +
+    geom_abline(intercept = 0,
+                slope = 1,
+                linetype = "dashed",
+                color = "black",
+                alpha = 0.7) +
+    ggtitle(metric) +
+    geom_text_repel(data = subset(all_classification_df, label_logic),
+                    arrow = arrow(length = unit(0.01, "npc")),
+                    box.padding = 0.6,
+                    point.padding = 0.3,
+                    segment.size = 0.2,
+                    segment.alpha = 0.6,
+                    size = 1.2,
+                    fontface = "italic",
+                    aes(label = target,
+                        x = AUROC_pycytominer,
+                        y = AUROC_cytominer)) +
+    theme_bw()
+
+outfile <- file.path("figures", paste0("compare_pycytominer_cytominer_", metric, ".png"))
+ggsave(outfile, height = 3, width = 3, dpi = 300)
+
+metric <- "AUPR"
+
+label_logic <- abs(all_classification_df$aupr_diff) > 0.08
+
+ggplot(all_classification_df,
+       aes(x=AUPR_pycytominer, 
+           y=AUPR_cytominer)) +
+    geom_point(alpha = 0.7,
+               size = 0.8) +
+    xlab("pycytominer") +
+    ylab("cytominer") +
+    geom_abline(intercept = 0,
+                slope = 1,
+                linetype = "dashed",
+                color = "black",
+                alpha = 0.7) +
+    ggtitle(metric) +
+    geom_text_repel(data = subset(all_classification_df, label_logic),
+                        arrow = arrow(length = unit(0.01, "npc")),
+                        box.padding = 0.6,
+                        point.padding = 0.3,
+                        segment.size = 0.2,
+                        segment.alpha = 0.6,
+                        size = 1.2,
+                        fontface = "italic",
+                        aes(label = target,
+                            x = AUPR_pycytominer,
+                            y = AUPR_cytominer)) +
+    theme_bw()
+
+outfile <- file.path("figures", paste0("compare_pycytominer_cytominer_", metric, ".png"))
+ggsave(outfile, height = 3, width = 3, dpi = 300)
+
+all_classification_df %>%
+    dplyr::mutate(percent_auroc_pycytominer_better = sum(auroc_diff > 0) / n(),
+                  percent_aupr_pycytominer_better = sum(aupr_diff > 0) / n()) %>%
+    dplyr::distinct(data_fit, shuffle, percent_auroc_pycytominer_better, percent_aupr_pycytominer_better)
