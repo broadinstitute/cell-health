76e214c2fff6b1b40d9685caf0c375a5763b6c92
gwaygenomics <gregory.way@gmail.com>
2019-11-19 12:06:43 -0500

    closes #77
    subset data to repurposing hub features

--- 3.train/scripts/nbconverted/0.stratify-data.py
+++ 3.train/scripts/nbconverted/0.stratify-data.py
@@ -41,6 +41,8 @@ data_dir = os.path.join("..", "1.generate-profiles", "data")
 file = os.path.join(data_dir, "consensus", "cell_painting_modz.tsv.gz")
 x_consensus_df = pd.read_csv(file, sep="\t")
 
+num_original_features = x_consensus_df.shape[1]
+
 print(x_consensus_df.shape)
 x_consensus_df.head(2)
 
@@ -55,9 +57,75 @@ print(y_consensus_df.shape)
 y_consensus_df.head(2)
 
 
-# ## Split into Training and Testing
+# ## Subset Features into those acquired in Repurposing Data
 
 # In[6]:
+
+
+# Note, these files are not yet public!
+repurposing_project_id = "2015_10_05_DrugRepurposing_AravindSubramanian_GolubLab_Broad"
+example_plate = "SQ00015058"
+
+repurposing_profile_dir = os.path.join(
+    "/Users",
+    "gway",
+    "work",
+    "projects",
+    repurposing_project_id,
+    "workspace",
+    "software",
+    repurposing_project_id,
+    "subsampling",
+    "data",
+    "profiles"
+)
+
+plate_dir = os.path.join(repurposing_profile_dir, example_plate, "n_all")
+example_plate_file = os.path.join(plate_dir, "{}_subsample_all_normalized.csv".format(example_plate))
+repurposing_df = pd.read_csv(example_plate_file)
+
+print(repurposing_df.shape)
+repurposing_df.head()
+
+
+# In[7]:
+
+
+cp_features = set(repurposing_df.columns[~repurposing_df.columns.str.startswith("Metadata")])
+cp_features = sorted(
+    list(
+        cp_features
+        .intersection(
+            set(
+                x_consensus_df.columns[~x_consensus_df.columns.str.startswith("Metadata")]
+            )
+        )
+    )
+)
+
+len(cp_features)
+
+
+# In[8]:
+
+
+meta_cols = x_consensus_df.columns[x_consensus_df.columns.str.startswith("Metadata")].tolist()
+x_consensus_df = x_consensus_df.loc[:, meta_cols + cp_features]
+num_subset_features = x_consensus_df.shape[1]
+
+print(x_consensus_df.shape)
+x_consensus_df.head()
+
+
+# In[9]:
+
+
+print("subsetting by repurposing features removed {} features".format(num_original_features - num_subset_features))
+
+
+# ## Split into Training and Testing
+
+# In[10]:
 
 
 x_train_df, x_test_df, y_train_df, y_test_df = train_test_split(
@@ -69,14 +137,14 @@ x_train_df, x_test_df, y_train_df, y_test_df = train_test_split(
 )
 
 
-# In[7]:
+# In[11]:
 
 
 print(x_train_df.shape)
 print(x_test_df.shape)
 
 
-# In[8]:
+# In[12]:
 
 
 file = os.path.join("data", "x_train.tsv.gz")
