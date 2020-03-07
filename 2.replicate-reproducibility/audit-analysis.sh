c102b333492acc6675f1538a81de805ec728795a
gwaygenomics <gregory.way@gmail.com>
2020-03-07 11:31:19 -0500

    complete analysis bash script

--- 2.replicate-reproducibility/audit-analysis.sh
+++ 2.replicate-reproducibility/audit-analysis.sh
@@ -5,6 +5,8 @@
 #
 # Audit Analysis Pipeline
 
+set -e
+
 # Step 0 - Convert all notebooks to scripts
 jupyter nbconvert --to=script --FilesWriter.build_directory=scripts/nbconverted *.ipynb
 
@@ -13,8 +15,18 @@ jupyter nbconvert --to=html \
         --FilesWriter.build_directory=scripts/html \
         --ExecutePreprocessor.kernel_name=python3 \
         --ExecutePreprocessor.timeout=10000000 \
-        --execute analyze-audit.ipynb
-        
-# Step 2 - Visualize platemaps
-Rscript --vanilla scripts/nbconverted/visualize-platemaps.r 
+        --execute 0.analyze-audit.ipynb
 
+# Step 2 - Visualize platemaps
+jupyter nbconvert --to=html \
+        --FilesWriter.build_directory=scripts/html \
+        --ExecutePreprocessor.kernel_name=ir \
+        --ExecutePreprocessor.timeout=10000000 \
+        --execute 1.visualize-platemaps.ipynb
+
+# Step 3 - Build morpheus gct files for heatmap generation
+jupyter nbconvert --to=html \
+        --FilesWriter.build_directory=scripts/html \
+        --ExecutePreprocessor.kernel_name=python3 \
+        --ExecutePreprocessor.timeout=10000000 \
+        --execute 2.build-morpheus-gct.ipynb
