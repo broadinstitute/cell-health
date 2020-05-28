2a18d5a5c813359a28120eaa41c7a8857b179d57
gwaygenomics <gregory.way@gmail.com>
2020-05-28 11:09:24 -0400

    update coefficient figures for different summary metrics

--- 3.train/scripts/nbconverted/8.visualize-coefficients.r
+++ 3.train/scripts/nbconverted/8.visualize-coefficients.r
@@ -377,151 +377,226 @@ for (target in unique(coef_df$target)) {
 
 dev.off()
 
-abs_weighted_max_df <- coef_df %>%
+coef_summary_df <- coef_df %>%
     dplyr::left_join(regression_metrics_df, by = c("target", "y_transform", "shuffle")) %>%
     dplyr::mutate(weighted_coef = weight * value, abs_weighted_coef = abs(weight * value)) %>%
     dplyr::group_by(feature, shuffle) %>%
-    dplyr::mutate(max_abs_weight_coef = max(abs_weighted_coef)) %>%
+    dplyr::mutate(
+        abs_max_weight = max(abs_weight),
+        abs_mean_weight = mean(abs_weight),
+        abs_95percentile = quantile(abs_weight, 0.95)
+    ) %>%
     dplyr::select(
-        feature, max_abs_weight_coef, compartment, feature_group, measurement, channel, parameter1, shuffle
+        feature,
+        abs_max_weight,
+        abs_mean_weight,
+        abs_95percentile,
+        compartment,
+        feature_group,
+        measurement,
+        channel,
+        parameter1,
+        shuffle
     ) %>%
     dplyr::distinct()
 
-head(abs_weighted_max_df)
+head(coef_summary_df)
 
 min_gradient <- 0
+summary_metrics <- c("abs_mean", "abs_max", "abs_95th")
 
 for (shuffle_option in c("Permuted", "Real")) {
-    max_df <- abs_weighted_max_df %>% dplyr::filter(shuffle == !!shuffle_option)
-    max_gradient <- max(max_df$max_abs_weight_coef)
+    summary_subset_df <- coef_summary_df %>% dplyr::filter(shuffle == !!shuffle_option)
     
-    # Process correlation different from other features
-    correlation_df <- max_df %>%
-        dplyr::filter(feature_group == "Correlation") %>%
-        dplyr::group_by(channel, parameter1, compartment, shuffle) %>%
-        dplyr::mutate(aggregated_max = max(max_abs_weight_coef)) %>%
-        dplyr::select(channel, parameter1, compartment, shuffle, aggregated_max) %>%
-        dplyr::distinct()
+    for (summary_metric in summary_metrics) {
+        
+        if (summary_metric == "abs_mean") {
+            column_choice <- "abs_mean_weight"
+            stat_string <- "aggregated_mean"
+            legend_name <- "Mean\nWeighted Coef"
+        } else if (summary_metric == "abs_max") {
+            column_choice <- "abs_max_weight"
+            stat_string <- "aggregated_max"
+            legend_name <- "Max\nWeighted Coef"
+        } else if (summary_metric == "abs_95th") {
+            column_choice <- "abs_95percentile"
+            stat_string <- "aggregated_95th"
+            legend_name <- "95th Percentile\nWeighted Coef"
+        }
+        
+        max_gradient <- max(summary_subset_df[, column_choice])
+        
+        # Process correlation different from other features
+        correlation_df <- summary_subset_df %>%
+            dplyr::filter(feature_group == "Correlation") %>%
+            dplyr::group_by(channel, parameter1, compartment, shuffle) %>%
+            dplyr::mutate(
+                aggregated_max = max(abs_max_weight),
+                aggregated_mean = mean(abs_mean_weight),
+                aggregated_95th = quantile(abs_95percentile, 0.95)
+            ) %>%
+            dplyr::select(
+                channel, parameter1, compartment, shuffle, aggregated_max, aggregated_mean, aggregated_95th
+            ) %>%
+            dplyr::distinct() %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate(
+                aggregated_max_round = round(aggregated_max, 2),
+                aggregated_mean_round = round(aggregated_mean * 100, 2),
+                aggregated_95th_round = round(aggregated_95th * 100, 2)
+            )
 
-    corr_comparments <- unique(correlation_df$compartment)
-    corr_channels <- unique(correlation_df$channel)
-    corr_feature_groups <- unique(correlation_df$parameter1)
-    corr_shuffle <- unique(correlation_df$shuffle)
-    corr_background <- tidyr::crossing(corr_comparments, corr_channels, corr_feature_groups, corr_shuffle)
-    colnames(corr_background) <- c("compartment", "channel", "parameter1", "shuffle")
-    
-    correlation_gg <- ggplot(correlation_df, aes(x = channel, y = parameter1)) +
-        geom_point(data = corr_background, fill="grey", size = 4, pch = 21) +
-        geom_point(aes(fill = aggregated_max), size = point_size, pch = 21) +
-        facet_wrap(~shuffle) +
-        scale_fill_gradient2(
-            name = "Max\nWeighted Coef",
-            low = "white",
-            high = "blue",
-            limits = c(min_gradient, max_gradient)
-        ) +
-        ylab("Channel Correlation") +
-        xlab("Channel Correlation") +
-        facet_grid(~compartment) + 
-        coord_fixed() +
-        geom_text(aes(label = round(aggregated_max, 2)), size = text_label_size) +
-        theme_bw() +
-        coef_theme +
-        theme(axis.text.x = element_text(angle = 90, hjust = 0.5))
-    
-    # Process area different from other features
-    area_df <- max_df %>%
-        dplyr::filter(feature_group %in% c("AreaShape", "Neighbors")) %>%
-        dplyr::group_by(compartment, feature_group, shuffle) %>%
-        dplyr::mutate(aggregated_max = max(max_abs_weight_coef)) %>%
-        dplyr::select(compartment, feature_group, shuffle, aggregated_max) %>%
-        dplyr::distinct()
-    
-    area_comparments <- unique(area_df$compartment)
-    area_feature_groups <- unique(area_df$feature_group)
-    area_shuffle <- unique(area_df$shuffle)
-    area_background <- tidyr::crossing(area_comparments, area_feature_groups, area_shuffle)
-    colnames(area_background) <- c("compartment", "feature_group", "shuffle")
+        corr_comparments <- unique(correlation_df$compartment)
+        corr_channels <- unique(correlation_df$channel)
+        corr_feature_groups <- unique(correlation_df$parameter1)
+        corr_shuffle <- unique(correlation_df$shuffle)
+        corr_background <- tidyr::crossing(corr_comparments, corr_channels, corr_feature_groups, corr_shuffle)
+        colnames(corr_background) <- c("compartment", "channel", "parameter1", "shuffle")
 
-    area_gg <- ggplot(area_df, aes(x = compartment, y = feature_group)) +
-        geom_point(data = area_background, fill="grey", size = 4, pch = 21) +
-        geom_point(aes(fill = aggregated_max), size = point_size, pch = 21) +
-        scale_fill_gradient2(
-            name = "Max\nWeighted Coef",
-            low = "white",
-            high = "blue",
-            limits = c(min_gradient, max_gradient)
-        ) +
-        ylab("Feature Group") +
-        xlab("Compartment") +
-        geom_text(aes(label = round(aggregated_max, 2)), size = text_label_size) +
-        theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) +
-        theme_bw() +
-        coord_fixed() +
-        coef_theme
-    
-    # Process compartment features different from other features
-    compartment_df <- max_df %>%
-        dplyr::filter(feature_group %in% !!compartment_features) %>%
-        dplyr::group_by(compartment, channel, feature_group, shuffle) %>%
-        dplyr::mutate(aggregated_max = max(max_abs_weight_coef)) %>%
-        dplyr::select(compartment, channel, feature_group, shuffle, aggregated_max) %>%
-        dplyr::distinct()
+        correlation_gg <- ggplot(correlation_df, aes(x = channel, y = parameter1)) +
+            geom_point(data = corr_background, fill="grey", size = 4, pch = 21) +
+            geom_point(aes_string(fill = stat_string), size = point_size, pch = 21) +
+            facet_wrap(~shuffle) +
+            scale_fill_gradient2(
+                name = legend_name,
+                low = "white",
+                high = "blue",
+                limits = c(min_gradient, max_gradient)
+            ) +
+            ylab("Channel Correlation") +
+            xlab("Channel Correlation") +
+            facet_grid(~compartment) + 
+            coord_fixed() +
+            geom_text(aes_string(label = paste0(stat_string, "_round")), size = text_label_size) +
+            theme_bw() +
+            coef_theme +
+            theme(axis.text.x = element_text(angle = 90, hjust = 0.5))
 
-    comp_comparments <- unique(compartment_df$compartment)
-    comp_channels <- unique(compartment_df$channel)
-    comp_feature_groups <- unique(compartment_df$feature_group)
-    comp_shuffle <- unique(compartment_df$shuffle)
-    comp_background <- tidyr::crossing(comp_comparments, comp_channels, comp_feature_groups, comp_shuffle)
-    colnames(comp_background) <- c("compartment", "channel", "feature_group", "shuffle")
+        # Process area different from other features
+        area_df <- summary_subset_df %>%
+            dplyr::filter(feature_group %in% c("AreaShape", "Neighbors")) %>%
+            dplyr::group_by(compartment, feature_group, shuffle) %>%
+            dplyr::mutate(
+                aggregated_max = max(abs_max_weight),
+                aggregated_mean = mean(abs_mean_weight),
+                aggregated_95th = quantile(abs_95percentile, 0.95)
+            ) %>%
+            dplyr::select(
+                compartment, feature_group, shuffle, aggregated_max, aggregated_mean, aggregated_95th
+            ) %>%
+            dplyr::distinct() %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate(
+                aggregated_max_round = round(aggregated_max, 2),
+                aggregated_mean_round = round(aggregated_mean * 100, 2),
+                aggregated_95th_round = round(aggregated_95th * 100, 2)
+            )
 
-    compartment_gg <- ggplot(compartment_df,
-                             aes(x = channel, y = feature_group)) +
-        geom_point(data = comp_background, fill="grey", size = 4, pch = 21) +
-        geom_point(aes(fill = aggregated_max), size = point_size, pch = 21) +
-        geom_text(aes(label = round(aggregated_max, 2)), size = text_label_size) +
-        facet_grid(~compartment) +
-        scale_fill_gradient2(
-            name = "Max\nWeighted Coef",
-            low = "white",
-            high = "blue",
-            limits = c(min_gradient, max_gradient)
-        ) +
-        ylab("Feature Group") +
-        xlab("Channel") +
-        coord_fixed() +
-        theme_bw() +
-        coef_theme +
-        theme(axis.text.x = element_text(angle = 90))
-    
-    # Compile full panel plot
-    bottom_panel <- cowplot::plot_grid(
-        correlation_gg + theme(legend.position = "none",
-                               plot.margin = margin(1.3, 1.3, 1.3, 1.3)),
-        compartment_gg + theme(legend.position = "none",
-                               plot.margin = margin(1.3, 1.3, 1.3, 1.3)),
-        nrow = 2,
-        align = "hv"
-        )
-    
-    legend_gg <- cowplot::get_legend(correlation_gg)
+        area_df$stat_output <- round(area_df[, stat_string], 2)
+        
+        area_comparments <- unique(area_df$compartment)
+        area_feature_groups <- unique(area_df$feature_group)
+        area_shuffle <- unique(area_df$shuffle)
+        area_background <- tidyr::crossing(area_comparments, area_feature_groups, area_shuffle)
+        colnames(area_background) <- c("compartment", "feature_group", "shuffle")
 
-    big_fig <- cowplot::plot_grid(
-        cowplot::plot_grid(
-            area_gg + theme(legend.position = "none"),
-            bottom_panel,
+        area_gg <- ggplot(area_df, aes(x = compartment, y = feature_group)) +
+            geom_point(data = area_background, fill = "grey", size = 4, pch = 21) +
+            geom_point(aes_string(fill = stat_string), size = point_size, pch = 21) +
+            scale_fill_gradient2(
+                name = legend_name,
+                low = "white",
+                high = "blue",
+                limits = c(min_gradient, max_gradient)
+            ) +
+            ylab("Feature Group") +
+            xlab("Compartment") +
+            geom_text(aes_string(label = paste0(stat_string, "_round")), size = text_label_size) +
+            theme(axis.text.y = element_text(angle = 90, hjust = 0.5)) +
+            theme_bw() +
+            coord_fixed() +
+            coef_theme
+
+        # Process compartment features different from other features
+        compartment_df <- summary_subset_df %>%
+            dplyr::filter(feature_group %in% !!compartment_features) %>%
+            dplyr::group_by(compartment, channel, feature_group, shuffle) %>%
+            dplyr::mutate(
+                aggregated_max = max(abs_max_weight),
+                aggregated_mean = mean(abs_mean_weight),
+                aggregated_95th = quantile(abs_95percentile, 0.95)
+            ) %>%
+            dplyr::select(
+                compartment, channel, feature_group, shuffle, aggregated_max, aggregated_mean, aggregated_95th
+            ) %>%
+            dplyr::distinct() %>%
+            dplyr::ungroup() %>%
+            dplyr::mutate(
+                aggregated_max_round = round(aggregated_max, 2),
+                aggregated_mean_round = round(aggregated_mean * 100, 2),
+                aggregated_95th_round = round(aggregated_95th * 100, 2)
+            )
+
+        compartment_df$stat_output <- round(compartment_df[, stat_string], 2)
+
+        comp_comparments <- unique(compartment_df$compartment)
+        comp_channels <- unique(compartment_df$channel)
+        comp_feature_groups <- unique(compartment_df$feature_group)
+        comp_shuffle <- unique(compartment_df$shuffle)
+        comp_background <- tidyr::crossing(comp_comparments, comp_channels, comp_feature_groups, comp_shuffle)
+        colnames(comp_background) <- c("compartment", "channel", "feature_group", "shuffle")
+
+        compartment_gg <- ggplot(compartment_df,
+                                 aes(x = channel, y = feature_group)) +
+            geom_point(data = comp_background, fill="grey", size = 4, pch = 21) +
+            geom_point(aes_string(fill = stat_string), size = point_size, pch = 21) +
+            geom_text(aes_string(label = paste0(stat_string, "_round")), size = text_label_size) +
+            facet_grid(~compartment) +
+            scale_fill_gradient2(
+                name = legend_name,
+                low = "white",
+                high = "blue",
+                limits = c(min_gradient, max_gradient)
+            ) +
+            ylab("Feature Group") +
+            xlab("Channel") +
+            coord_fixed() +
+            theme_bw() +
+            coef_theme +
+            theme(axis.text.x = element_text(angle = 90))
+
+        # Compile full panel plot
+        bottom_panel <- cowplot::plot_grid(
+            correlation_gg + theme(legend.position = "none",
+                                   plot.margin = margin(1.3, 1.3, 1.3, 1.3)),
+            compartment_gg + theme(legend.position = "none",
+                                   plot.margin = margin(1.3, 1.3, 1.3, 1.3)),
             nrow = 2,
-            rel_heights = c(0.3, 1)
-        ),
-        legend_gg,
-        ncol = 2,
-        rel_widths = c(1, 0.15)
-    )
-    
-    print(big_fig)
+            align = "hv"
+            )
 
-    output_file <- file.path(
-        coef_dir, paste0("coefficient_summary_", shuffle_option, "_", consensus, ".png")
-    )
-    cowplot::save_plot(output_file, big_fig, base_height = 5, base_width = 6, dpi = 500)
+        legend_gg <- cowplot::get_legend(correlation_gg)
+
+        big_fig <- cowplot::plot_grid(
+            cowplot::plot_grid(
+                area_gg + theme(legend.position = "none"),
+                bottom_panel,
+                nrow = 2,
+                rel_heights = c(0.3, 1)
+            ),
+            legend_gg,
+            ncol = 2,
+            rel_widths = c(1, 0.15)
+        )
+
+        print(big_fig)
+
+        output_file <- file.path(
+            coef_dir,
+            paste0(
+                "coefficient_summary_", shuffle_option, "_", consensus, "_", summary_metric, ".png"
+            )
+        )
+        cowplot::save_plot(output_file, big_fig, base_height = 5, base_width = 6, dpi = 500)
+    }
 }
