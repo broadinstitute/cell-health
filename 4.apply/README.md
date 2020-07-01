5d55961ba3a02c73d17e61123153818ad07ed69f
gwaygenomics <gregory.way@gmail.com>
2020-07-01 17:00:58 -0400

    add pipeline to apply module

---
+++ 4.apply/README.md
@@ -0,0 +1,6 @@
+# Applying Cell Health Models to the Drug Repurposing Hub
+
+**Gregory Way, 2020**
+
+The Drug Repurposing Hub project generated Cell Painting data for a subset of compounds.
+For complete processing details, see this repository: https://github.com/broadinstitute/lincs-cell-painting
---
+++ 4.apply/apply-models.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+##############################################
+# Applying trained models to Cell Painting Data from
+# The Drug Repurposing Hub
+#
+# Gregory Way, 2020
+##############################################
+
+# Step 0: Convert all notebooks to scripts
+jupyter nbconvert --to=script \
+        --FilesWriter.build_directory=scripts/nbconverted \
+        *.ipynb
+
+
+
+# Step 2: Visualize results
+jupyter nbconvert --to=html \
+        --FilesWriter.build_directory=scripts/html \
+        --ExecutePreprocessor.kernel_name=ir \
+        --ExecutePreprocessor.timeout=10000000 \
+        --execute 1.visualize-repurposing
+
+# Step 3: Fit dose curves
+Rscript --vanilla 2.fit-dose.R
+
+# Step 4: Visualize dose response curves
+jupyter nbconvert --to=html \
+        --FilesWriter.build_directory=scripts/html \
+        --ExecutePreprocessor.kernel_name=ir \
+        --ExecutePreprocessor.timeout=10000000 \
+        --execute 3.visualize-dose-response.ipynb
+
+# Step 5: Generate summary figures
+jupyter nbconvert --to=html \
+        --FilesWriter.build_directory=scripts/html \
+        --ExecutePreprocessor.kernel_name=ir \
+        --ExecutePreprocessor.timeout=10000000 \
+        --execute 4.lincs-figures.ipynb
